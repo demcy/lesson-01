c8a096ec1371086b879e9fdf690d0352ae8d5a2b
Aleksandr Khokhlachev <demcy.meizu@gmail.com>
2024-11-18 17:06:44 +0200

    add lesson 4

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,20 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './app/store.jsx'
 import { fetchUsers } from './features/users/usersSlice.jsx'
+import { fetchPosts } from './features/posts/postsSlice.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 store.dispatch(fetchUsers());
+store.dispatch(fetchPosts());
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <Routes>
+          <Route path='/*' element={<App />} />
+        </Routes>
+      </Router>
     </Provider>
   </StrictMode>,
 )
