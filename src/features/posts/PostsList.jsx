548b146f2ad86d2e6c8b43a30681d8633bea2b4a
Aleksandr Khokhlachev <demcy.meizu@gmail.com>
2024-11-18 23:33:38 +0200

    lesson complete

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -1,10 +1,10 @@
 import { useSelector } from "react-redux";
-import { selectAllPosts, getPostsStatus, getPostsError } from "./postsSlice";
+import { selectPostIds, getPostsStatus, getPostsError } from "./postsSlice";
 import PostsExcerpt from "./PostsExcerpt";
 
 const PostsList = () => {
 
-    const posts = useSelector(selectAllPosts);
+    const orderedPostIds = useSelector(selectPostIds)
     const postStatus = useSelector(getPostsStatus);
     const error = useSelector(getPostsError);
 
@@ -12,16 +12,7 @@ const PostsList = () => {
     if (postStatus === 'loading') {
         content = <p>"Loading..."</p>;
     } else if (postStatus === 'succeeded') {
-        const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
-        const preContent = orderedPosts.filter((val, i) => {
-            if (i !== orderedPosts.length - 1) {
-              return val.id !== orderedPosts[i + 1].id
-            }
-            return val
-          })
-          content = preContent.map((post) => (
-            <PostsExcerpt key={post.id} post={post} />
-          ))
+        content = orderedPostIds.map(postId => <PostsExcerpt key={postId} postId={postId} />)
     } else if (postStatus === 'failed') {
         content = <p>{error}</p>;
     }
